07ac8fa2cfe4993f7d73515415778a1221f19029
hyunsuk01 <76142195+hyunsuk01@users.noreply.github.com>
2024-09-24 11:56:42 +0900

    캐릭터 화면 출력 구현

--- character_grass.py
+++ character_grass.py
@@ -7,6 +7,11 @@ character = load_image('character.png')
 
 def run_circle():
     print('CIRCLE')
+
+    clear_canvas_now()
+    character.draw_now(400, 300)
+    delay(0.1)
+    
     pass
 
 def run_rectangle():
@@ -16,6 +21,7 @@ def run_rectangle():
 while True:
     run_circle()
     run_rectangle()
+    break
     
 
 close_canvas()
